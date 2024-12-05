2bbe7f92e95eff2ec957e30f755d4319860074fd
Nisarg208 <69039339+Nick-exe17@users.noreply.github.com>
2024-12-05 07:35:56 -0500

    Update main.tf
    Open NodePort (30001) as an ingress to the security group.

--- terraform/dev/instances/main.tf
+++ terraform/dev/instances/main.tf
@@ -101,6 +101,15 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  ingress {
+    description      = "NodePort (30001) from everywhere"
+    from_port        = 30001
+    to_port          = 30001
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 #ECR repositories
